92d84d1777d39445a5a5003fb4e5d1dbdb20717d
Leonardo Gentile <leonard.gentile@gmail.com>
2018-10-20 01:06:45 +0200

    postcss config

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+// const tailwindcss = require('tailwindcss');
+// const lost = require('lost');
+
+// See: https://medium.com/@mikeeeeeeey/create-react-app-tailwind-css-feat-postcss-631d9e33ba8c
+
+module.exports = {
+  plugins: [
+    // tailwindcss('./tailwind.config.js'),
+    // lost
+  ]
+};
